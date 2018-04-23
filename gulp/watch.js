ab7d1cee765b992d59bb7be0c6644a18efac052c
Adham Moussa <adam.moussa62017@gmail.com>
2018-04-23 03:32:21 +0200

    Completed header style

---
+++ gulp/watch.js
@@ -0,0 +1,23 @@
+let gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('watch', function () {
+    browserSync.init({
+        notify: false,
+        server: 'app'
+    });
+
+    watch('./app/index.html', function () {
+        browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.scss', function () {
+        gulp.start('cssInject');
+    });
+});
+
+gulp.task('cssInject', ['styles'], function () {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
